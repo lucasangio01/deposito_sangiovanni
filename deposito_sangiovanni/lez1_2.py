3029b7ed19682a75fce40dddc7af0fd0920ffa76
Luca Sangiovanni <luca.sangiovanni.2001@gmail.com>
2025-09-11 18:03:25 +0200

    Add last parts of code

--- deposito_sangiovanni/lez1_2.py
+++ deposito_sangiovanni/lez1_2.py
@@ -5,28 +5,33 @@ from sklearn.ensemble import RandomForestClassifier
 from imblearn.over_sampling import SMOTE
 
 
+# Load the csv
 def load_data(path):
     data = pd.read_csv(path)
     return data
 
+# Split data into train and test
 def split_data(data):
     X = data.drop("Class", axis = 1)
     y = data["Class"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.20, random_state=42)
     return X_train, X_test, y_train, y_test
 
+# Create a decision tree classifier
 def make_predictions_tree(X_train, y_train, X_test):
     tree = DecisionTreeClassifier(max_depth=5, class_weight="balanced" ,random_state=42)
     tree.fit(X_train, y_train)
     y_pred = tree.predict(X_test)
     return y_pred
 
+# Create a random forest classifier
 def make_predictions_rf(X_train, y_train, X_test):
     rf = RandomForestClassifier(n_estimators=50, max_depth=5, class_weight="balanced", random_state=42)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_test)
     return y_pred
 
+# Perform oversampling using SMOTE
 def oversample(X_train, y_train):
     smote = SMOTE()
     X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
